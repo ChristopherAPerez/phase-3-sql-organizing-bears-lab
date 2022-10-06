e30e1eef21078216c62542ee9341ed030a4b91c3
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-10-06 17:49:54 -0400

    commit tree f9413d6fb11bb469c3861e35731918dbdd7dac44

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'red', 'sassy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'F', 'blue', 'sassy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'M', 'white', 'sassy', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'brown', 'sassy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'red', 'sassy', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'grey', 'sassy', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'red', 'sassy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, 'F', 'green', 'sassy', true);
